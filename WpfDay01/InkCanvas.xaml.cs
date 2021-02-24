a61195aade329db141f1eb98c582706cdc7250ca
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-24 15:33:42 +0200

    Initial commit

---
+++ WpfDay01/InkCanvas.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDay01
+{
+    /// <summary>
+    /// Interaction logic for InkCanvas.xaml
+    /// </summary>
+    public partial class InkCanvas : Window
+    {
+        public InkCanvas()
+        {
+            InitializeComponent();
+
+          
+        }
+
+       // System.Windows.Controls.InkCanvas
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            switch(((RadioButton)sender).Content.ToString())
+            {
+                case "Red":
+                    ink.DefaultDrawingAttributes.Color = Colors.Red;
+                    break;
+                case "Green":
+                    ink.DefaultDrawingAttributes.Color = Colors.Green;
+                    break;
+                case "Blue":
+                    ink.DefaultDrawingAttributes.Color = Colors.Blue;
+                    break;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "Ink":
+                    ink.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Select":
+                    ink.EditingMode = InkCanvasEditingMode.Select;
+
+                    break;
+                case "Erase":
+                    ink.EditingMode = InkCanvasEditingMode.EraseByPoint;
+
+                    break;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ink.Strokes.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ink.CopySelection();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ink.CutSelection();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ink.Paste();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            FileStream f = new FileStream("D:/canvas.ink", FileMode.Create, FileAccess.Write);
+            ink.Strokes.Save(f);
+            f.Close();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+            FileStream r = new FileStream("D:/canvas.ink", FileMode.Open, FileAccess.Read);
+            StrokeCollection strokes = new StrokeCollection(r);
+            ink.Strokes = strokes;
+            r.Close();
+        }
+    }
+}
